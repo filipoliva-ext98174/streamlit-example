e3b19be195978edecc15fd684ee46d50d8592d39
Filip Oliva <112169843+filipoliva-ext98174@users.noreply.github.com>
2024-04-29 15:36:29 +0200

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy  as np
 import pandas as pd
 import json
+from streamlit.web.server.websocket_headers import _get_websocket_headers
 
 # Set the app title 
 st.title('Test editace metadat') 
